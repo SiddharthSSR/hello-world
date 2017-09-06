71a7c87d4b9a9002509ff65fcf48241d684ab8af
SiddharthSSR <31690690+SiddharthSSR@users.noreply.github.com>
2017-09-06 15:09:03 +0530

    Add files via upload

---
+++ hello.js
@@ -0,0 +1,8 @@
+function setup(){
+	createCanvas(600, 600);
+	console.log("Hello World");
+}
+
+function draw(){
+	background(51);
+}
---
+++ hello.py
@@ -0,0 +1,4 @@
+def printing():
+	print("Hello World")
+
+printing()
